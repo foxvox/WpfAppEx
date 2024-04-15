acb6a2c275146251089fa5cec2f1026efba78ff6
alex <windev@nate.com>
2024-04-16 00:15:28 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppEx
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(textBox.Text + "님 환영합니다.", "Hello world"); 
+		}
+
+		private void MouseLBtnUp(object sender, MouseButtonEventArgs e)
+		{
+			MessageBox.Show("Hi There", "Hello world", MessageBoxButton.OK, MessageBoxImage.Information); 
+		}
+	}
+}
